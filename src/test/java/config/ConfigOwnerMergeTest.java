dace660cf23347cc4302f99c057ac42a0b8369ce
svasenkov <aanher@gmail.com>
2026-07-02 02:57:15 +0200

    test(unit): Owner system property overrides

---
+++ src/test/java/config/ConfigOwnerMergeTest.java
@@ -0,0 +1,30 @@
+package config;
+
+import annotations.Layer;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Layer("unit")
+@DisplayName("Owner config merge")
+class ConfigOwnerMergeTest {
+
+    @Test
+    @DisplayName("system property hubUrl overrides file default")
+    void systemPropertyOverridesFileDefault() {
+        var previous = System.getProperty("hubUrl");
+        try {
+            System.setProperty("hubUrl", "http://override.example.com:4444");
+            var config = ConfigFactory.create(TestConfig.class);
+            assertEquals("http://override.example.com:4444", config.hubUrl());
+        } finally {
+            if (previous == null) {
+                System.clearProperty("hubUrl");
+            } else {
+                System.setProperty("hubUrl", previous);
+            }
+        }
+    }
+}
